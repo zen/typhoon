347dc635f813ef7a585de317bf4b3dc17d993e16
Marcin Karkocha <marcin.karkocha@outlook.com>
2020-11-05 15:02:00 +0100

    switch all coreos to same

--- aws/container-linux/kubernetes/workers/ami.tf
+++ aws/container-linux/kubernetes/workers/ami.tf
@@ -2,30 +2,8 @@ locals {
   # Pick a CoreOS Container Linux derivative
   # coreos-stable -> Container Linux AMI
   # flatcar-stable -> Flatcar Linux AMI
-  ami_id = local.flavor == "flatcar" ? data.aws_ami.flatcar.image_id : data.aws_ami.coreos.image_id
-
-  flavor  = split("-", var.os_image)[0]
+  ami_id = data.aws_ami.flatcar.image_id
   channel = split("-", var.os_image)[1]
-}
-
-data "aws_ami" "coreos" {
-  most_recent = true
-  owners      = ["595879546273"]
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "name"
-    values = ["CoreOS-${local.flavor == "coreos" ? local.channel : "stable"}-*"]
-  }
 }
 
 data "aws_ami" "flatcar" {
